3c6bb13b98627aed6f7928e49382d96cf9e0ddf4
bodynight <xbodynightx@gmail.com>
2021-06-01 12:33:55 +0300

    Подтянул с RubySchool

--- app.rb
+++ app.rb
@@ -4,28 +4,30 @@ require 'sinatra/reloader'
 require 'sqlite3'
 
 def get_db
-	return SQLite3::Database.new 'barbershop.db'
+	db = SQLite3::Database.new 'barbershop.db'
+	db.results_as_hash = true
+	return db
 end
 
 configure do
-		db=get_db 
-		db.execute 'CREATE TABLE IF NOT EXISTS "Users" (
-    "id"        INTEGER PRIMARY KEY AUTOINCREMENT,
-    "username"  TEXT,
-    "phone"     TEXT,
-    "datestamp" TEXT,
-    "barber"    TEXT,
-    "color"     TEXT
-)'
-
-	end
+	db = get_db
+	db.execute 'CREATE TABLE IF NOT EXISTS
+		"Users"
+		(
+			"id" INTEGER PRIMARY KEY AUTOINCREMENT,
+			"username" TEXT,
+			"phone" TEXT,
+			"datestamp" TEXT,
+			"barber" TEXT,
+			"color" TEXT
+		)'
+end
 
 get '/' do
 	erb "Hello! <a href=\"https://github.com/bootstrap-ruby/sinatra-bootstrap\">Original</a> pattern has been modified for <a href=\"http://rubyschool.us/\">Ruby School</a>"			
 end
 
 get '/about' do
-	
 	erb :about
 end
 
@@ -46,24 +48,33 @@ post '/visit' do
 			:phone => 'Введите телефон',
 			:datetime => 'Введите дату и время' }
 
-	@error = hh.select {|key,valuesfgh| params[key] == ""}.values.join(", ")
-
-	
+	@error = hh.select {|key,_| params[key] == ""}.values.join(", ")
 
 	if @error != ''
 		return erb :visit
 	end
 
 	db = get_db
-	db.execute 'insert into Users (
-		 username, phone, datestamp,
-		 barber, color)
-		 values ( ?, ?, ?, ?, ?)', [@username, @phone, @datetime,@barber,@color]
+	db.execute 'insert into
+		Users
+		(
+			username,
+			phone,
+			datestamp,
+			barber,
+			color
+		)
+		values (?, ?, ?, ?, ?)', [@username, @phone, @datetime, @barber, @color]
 
 	erb "OK, username is #{@username}, #{@phone}, #{@datetime}, #{@barber}, #{@color}"
 
 end
 
 get '/showusers' do
- erb "Hello World"
+	db = get_db
+
+	@results = db.execute 'select * from Users order by id desc'
+
+	erb :showusers
 end
+
